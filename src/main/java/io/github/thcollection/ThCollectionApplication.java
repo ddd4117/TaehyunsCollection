e6a7c4d3529d370bf0cc8c3dfb63683fdbc706ee
Taehyun <ddd2520@naver.com>
2020-03-02 22:10:07 +0900

    프로젝트 시작

---
+++ src/main/java/io/github/thcollection/ThCollectionApplication.java
@@ -0,0 +1,13 @@
+package io.github.thcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThCollectionApplication.class, args);
+    }
+
+}
